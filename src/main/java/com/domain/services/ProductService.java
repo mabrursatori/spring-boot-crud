6fd3e159e937d50e0d9e00a36ca6532a420268c3
mabrursatori <mabrursatori@gmail.com>
2021-09-27 16:32:03 +0700

    First Commit

---
+++ src/main/java/com/domain/services/ProductService.java
@@ -0,0 +1,79 @@
+package com.domain.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import com.domain.models.entities.Product;
+import com.domain.models.entities.Supplier;
+import com.domain.models.repos.ProductRepo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class ProductService {
+
+    @Autowired
+    private ProductRepo productRepo;
+
+    @Autowired
+    private SupplierService supplierService;
+
+    public Product save(Product product) {
+        return productRepo.save(product);
+    }
+
+    public Product findOne(Long id) {
+        Optional<Product> product = productRepo.findById(id);
+        if (!product.isPresent()) {
+            return null;
+        }
+        return product.get();
+    }
+
+    public Iterable<Product> findAll() {
+        return productRepo.findAll();
+    }
+
+    public void removeOne(Long id) {
+        productRepo.deleteById(id);
+    }
+
+    public List<Product> findByName(String name) {
+        return productRepo.findByNameContains(name);
+    }
+
+    public void addSupplier(Supplier supplier, Long productId) {
+        Product product = findOne(productId);
+        if (product == null) {
+            throw new RuntimeException("Product with ID: " + productId + " not found");
+        }
+        product.getSuppliers().add(supplier);
+        save(product);
+    }
+
+    public Product findByProductName(String name) {
+        return productRepo.findProductByName(name);
+    }
+
+    public List<Product> findByProductNameLike(String name) {
+        return productRepo.findByProductNameLike("%" + name + "%");
+    }
+
+    public List<Product> findByCategory(Long categoryId) {
+        return productRepo.findProductByCategory(categoryId);
+    }
+
+    public List<Product> findBySupplier(Long SupplierId){
+        Supplier supplier = supplierService.findOne(SupplierId);
+        if(supplier == null){
+            return new ArrayList<Product>();
+        }
+        return productRepo.findProductBySupplier(supplier);
+    }
+
+}
---
+++ src/main/java/com/domain/services/SupplierService.java
@@ -0,0 +1,56 @@
+package com.domain.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import com.domain.models.entities.Supplier;
+import com.domain.models.repos.SupplierRepo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class SupplierService {
+
+    @Autowired
+    private SupplierRepo supplierRepo;
+
+    public Supplier save(Supplier supplier) {
+        return supplierRepo.save(supplier);
+    }
+
+    public Supplier findOne(Long id) {
+        Optional<Supplier> supplier = supplierRepo.findById(id);
+        if (!supplier.isPresent()) {
+            return null;
+        }
+        return supplier.get();
+    }
+
+    public Iterable<Supplier> findAll() {
+        return supplierRepo.findAll();
+    }
+
+    public void removeOne(Long id) {
+        supplierRepo.deleteById(id);
+    }
+
+    public Supplier findByEmail(String email) {
+        return supplierRepo.findByEmail(email);
+    }
+
+    public List<Supplier> findByName(String name) {
+        return supplierRepo.findByNameContainsOrderByIdDesc(name);
+    }
+
+    public List<Supplier> findByNameStartWith(String prefix) {
+        return supplierRepo.findByNameStartingWith(prefix);
+    }
+
+    public List<Supplier> findByNameOrEmail(String name, String email) {
+        return supplierRepo.findByNameContainsOrEmailContains(name, email);
+    }
+}
